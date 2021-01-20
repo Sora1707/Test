0e005113e9976514cb0a46223fdc8837a661c27d
Sora1707 <volan1301@gmail.com>
2021-01-20 09:14:01 +0700

    Initial commit

---
+++ test.py
@@ -0,0 +1,20 @@
+
+import math
+import os
+import random
+import re
+import sys
+import textwrap
+
+#print("{:.2f}".format(ans))
+#textwrap.fill() / wrap()
+'''
+def fun(N):
+    width = len(bin(N)) - 2
+    for i in range(1,N+1):
+        print ("{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width = width))
+
+n = int(input())
+fun(n)'''
+
+print("{x}".format(x = 12))
